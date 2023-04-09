2ca3576baecc5ebee9c311f75ea67da163350ded
Mior Mior Saifullizan <mmio0004@student.monash.edu>
2023-04-09 20:57:30 +0800

    Added code to show Monash University logo and position in middle

--- frontend/src/components/LoginPage.tsx
+++ frontend/src/components/LoginPage.tsx
@@ -1,69 +1,77 @@
 // LoginPage.tsx
 import React, { useState, FormEvent } from 'react';
 import {
-  Box,
-  VStack,
-  Input,
-  FormControl,
-  FormLabel,
-  Button,
-  Text,
-  useToast,
+Box,
+VStack,
+Input,
+FormControl,
+FormLabel,
+Button,
+Text,
+useToast,
+Image,
+Center,
 } from '@chakra-ui/react';
 
 interface LoginProps {
-  onLoginSuccess: () => void;
+onLoginSuccess: () => void;
 }
 
 const LoginPage: React.FC<LoginProps> = ({ onLoginSuccess }) => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const toast = useToast();
-  const handleSubmit = (e: FormEvent) => {
+const [email, setEmail] = useState('');
+const [password, setPassword] = useState('');
+const toast = useToast();
+const handleSubmit = (e: FormEvent) => {
     e.preventDefault();
     toast({
-      title: 'Logged In',
-      description: `Welcome, ${email}`,
-      status: 'success',
-      duration: 3000,
-      isClosable: true,
+    title: 'Logged In',
+    description: `Welcome, ${email}`,
+    status: 'success',
+    duration: 3000,
+    isClosable: true,
     });
     onLoginSuccess();
-  };
+};
 
-  return (
+return (
     <Box width="100%" maxWidth="400px" margin="0 auto" mt="100px">
-      <VStack
+        <Center mb={4}>
+            <Image
+                src="https://media.discordapp.net/attachments/950381894187483173/1003982545299439778/2016-MonashUniversityMALAYSIA_2-MonoCMYKoutlines.png?width=653&height=378"
+                alt="Monash University Logo"
+            />
+        </Center>
+        <VStack
         as="form"
         onSubmit={handleSubmit}
         alignItems="stretch"
         spacing={4}
-      >
+        >
         <Text fontSize="2xl" fontWeight="bold" textAlign="center" mb={4}>
-          Login
+            Login
         </Text>
         <FormControl id="email">
-          <FormLabel>Email</FormLabel>
-          <Input
+        <FormLabel>Email</FormLabel>
+        <Input
             type="email"
             value={email}
             onChange={(e) => setEmail(e.target.value)}
-          />
+        />
         </FormControl>
         <FormControl id="password">
-          <FormLabel>Password</FormLabel>
-          <Input
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
+        <FormLabel>Password</FormLabel>
+        <Input
+        type="password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+        />
         </FormControl>
         <Button type="submit" colorScheme="blue" mt={4}>
-          Login
+            Login
         </Button>
-      </VStack>
+        </VStack>
     </Box>
-  );
+);
 };
 
 export default LoginPage;
