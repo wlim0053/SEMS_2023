1dedbc8ea01868014ec3b84f23a791cf85e31398
Mior Mior Saifullizan <mmio0004@student.monash.edu>
2023-04-12 10:06:43 +0800

    Removed unnecessary code lines in App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,20 +4,20 @@ import { ChakraProvider } from '@chakra-ui/react';
 // components
 import LoginPage from './components/LoginPage';
 
-const App: React.FC = () => {
+const App = () => {
 const [isLoggedIn, setIsLoggedIn] = useState(false);
 const handleLoginSuccess = () => {
     setIsLoggedIn(true);
   };
 
   return (
-    <ChakraProvider>
+    <>
       {!isLoggedIn ? (
         <LoginPage onLoginSuccess={handleLoginSuccess} />
       ) : (
         <div>Logged in successfully!</div>
       )}
-    </ChakraProvider>
+    </>
   );
 };
 
