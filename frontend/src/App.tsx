330b4d9bf554371da36ea66660881024c3ba9b7d
Lohan Vidanage <lvid0003@student.monash.edu>
2023-04-14 06:46:52 +0800

    Added routing to access student home page.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,20 +1,32 @@
 // hooks
-import { useState } from 'react'
+import React from "react";
 // components
-import Navbar from './components/Navbar'
-import Admin from './pages/Admin'
+import Navbar from "./components/Navbar";
+import Admin from "./pages/Admin";
 // css
-import './App.css'
-
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import StudentHome from "./pages/StudentHome";
 
 function App() {
-
-  	return (
-    	<div className="App">
-      		<Navbar></Navbar>
-      		<Admin></Admin>
-    	</div>
-  )
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <>
+                <Navbar />
+                <Admin />
+              </>
+            }
+          />
+          <Route path="/StudentHome" element={<StudentHome />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
-export default App
+export default App;
