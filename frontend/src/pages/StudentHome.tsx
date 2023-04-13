affcdc85ea1e28c392025cff38dd1dae5ed97ee6
Lohan Vidanage <lvid0003@student.monash.edu>
2023-04-14 06:51:02 +0800

    Create StudentHome.tsx

---
+++ frontend/src/pages/StudentHome.tsx
@@ -0,0 +1,15 @@
+// stub code
+import Navbar from "../components/Navbar";
+import React from "react";
+import Calendar from "../components/Calendar";
+
+function StudentHome() {
+  return (
+    <div>
+      <Navbar />
+      <Calendar />
+    </div>
+  );
+}
+
+export default StudentHome;
