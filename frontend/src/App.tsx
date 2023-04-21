fc0fdd09aeac513df928c2c808e7a2192559496f
Kennedy Tan <ktan0087@student.monash.edu>
2023-04-22 00:53:31 +0800

    Update AdminDashboard file

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -8,11 +8,7 @@ import './App.css'
 import { BrowserRouter as Router, Route, Outlet } from "react-router-dom";
 import OrganiserList from './pages/Admin/OrganiserList';
 import ActivityLog from './pages/Admin/ActivityLog';
-// hooks
-import { useState } from "react";
-// components
-import Navbar from "./components/Navbar";
-import Admin from "./pages/Admin";
+
 import { Box } from "@chakra-ui/react";
 // css
 
