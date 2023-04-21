80996bd5484e3c31e491278cdbe21f4b85102f4b
Kennedy Tan <ktan0087@student.monash.edu>
2023-04-22 00:29:10 +0800

    Add import from chakra ui

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,20 +1,18 @@
 // hooks
-import { useState } from 'react'
+import { useState } from "react";
 // components
-import Navbar from './components/Navbar'
-import Admin from './pages/Admin'
+import Navbar from "./components/Navbar";
+import Admin from "./pages/Admin";
+import { Box } from "@chakra-ui/react";
 // css
-import './App.css'
-
 
 function App() {
-
-  	return (
-    	<div className="App">
-      		<Navbar></Navbar>
-      		<Admin></Admin>
-    	</div>
-  )
+  return (
+    <div className="App">
+      <Navbar></Navbar>
+      <Admin></Admin>
+    </div>
+  );
 }
 
-export default App
+export default App;
