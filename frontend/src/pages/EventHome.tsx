e65515d1c61fa01aee593ce0d4d7c8c9666fe6d7
Lohan Vidanage <lvid0003@student.monash.edu>
2023-05-01 17:26:31 +0800

    Added EventHome.tsx which has the EventPage.tsx (Events table)

---
+++ frontend/src/pages/EventHome.tsx
@@ -0,0 +1,14 @@
+import Navbar from "../components/Navbar";
+import React from "react";
+import EventPage from "../components/EventPage";
+
+function EventHome() {
+  return (
+    <div>
+      <Navbar />
+      <EventPage />
+    </div>
+  );
+}
+
+export default EventHome;
