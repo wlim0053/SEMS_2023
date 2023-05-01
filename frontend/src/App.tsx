99b9c04f93c3fd9b4933b775fcedebceb0d9b4ba
Anson Wong Sie Yuan <awon0048@student.monash.edu>
2023-05-01 23:21:45 +0800

    Updated new Landing Page with chakra UI

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,16 +3,18 @@ import { useState } from 'react'
 // components
 import Navbar from './components/Navbar'
 import Admin from './pages/Admin'
+import Testlandingpage from './components/Testlandingpage'
 // css
 import './App.css'
 
 
+//<Navbar></Navbar>
+//<Admin></Admin>
 function App() {
 
   	return (
     	<div className="App">
-      		<Navbar></Navbar>
-      		<Admin></Admin>
+			<Testlandingpage></Testlandingpage>
     	</div>
   )
 }
