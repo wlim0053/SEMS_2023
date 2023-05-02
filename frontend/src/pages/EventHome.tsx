a11f31fc68f775eda5144662049026531efc3541
Lohan Vidanage <lvid0003@student.monash.edu>
2023-05-02 11:30:10 +0800

    Renoved Navar component, as it wasn't needed.

--- frontend/src/pages/EventHome.tsx
+++ frontend/src/pages/EventHome.tsx
@@ -1,11 +1,9 @@
-import Navbar from "../components/Navbar";
 import React from "react";
 import EventPage from "../components/EventPage";
 
 function EventHome() {
   return (
     <div>
-      <Navbar />
       <EventPage />
     </div>
   );
