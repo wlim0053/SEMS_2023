814ee8f47bb89b75ff2e5767cb772996baeed0b8
Lohan Vidanage <lvid0003@student.monash.edu>
2023-05-02 11:29:28 +0800

    Updated Router linking methof.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,32 +1,18 @@
 // hooks
-import React from "react";
 // components
-import Navbar from "./components/Navbar";
-import Admin from "./pages/Admin";
+import Navbar from './components/Navbar'
 // css
-import "./App.css";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import StudentHome from "./pages/StudentHome";
-import EventHome from "./pages/EventHome";
+import './App.css'
+import { BrowserRouter as Router, Outlet } from "react-router-dom";
+// css
 
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
-        <Routes>
-          <Route
-            path="/"
-            element={
-              <>
-                <Navbar />
-                <Admin />
-              </>
-            }
-          />
-          <Route path="/StudentHome" element={<StudentHome />} />
-          <Route path="/EventHome" element={<EventHome />} />
-        </Routes>
-      </BrowserRouter>
+      <Router>
+        <Navbar />
+        <Outlet /> {/* Render nested routes */}
+      </Router>
     </div>
   );
 }
