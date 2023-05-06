4712d99fb2d0f005318de82b64858d50fd63c448
wlim0053 <wlim0053@student.monash.edu>
2023-05-06 18:07:59 +0800

    extracted database config to dbConfig.ts

--- backend/src/utils/dfConfig.ts
+++ backend/src/utils/dfConfig.ts
@@ -4,7 +4,7 @@ dotenv.config({
 	path: "development" === process.env.NODE_ENV ? ".env.dev" : ".env.prod",
 })
 
-export const dbConfig: mssql.config = {
+const dbConfig: mssql.config = {
 	user: process.env.sa,
 	password: process.env.password,
 	server: process.env.server || "",
@@ -16,3 +16,5 @@ export const dbConfig: mssql.config = {
 	},
 	port: Number(process.env.db_port) || 1433,
 }
+
+export const pool = new mssql.ConnectionPool(dbConfig)
