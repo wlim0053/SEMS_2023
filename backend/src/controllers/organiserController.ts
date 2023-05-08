30bcb3e05b2ea9256e95581203f15f7ccd2cedcf
wlim0053 <wlim0053@student.monash.edu>
2023-05-09 05:40:13 +0800

    renamed file and updated imports

--- backend/src/controllers/organiserController.ts
+++ backend/src/controllers/organiserController.ts
@@ -1,18 +1,24 @@
 import { Request, Response } from "express"
 import mssql from "mssql"
 import { pool } from "../utils/dbConfig"
-import { StatusCodes } from "../utils/statusCodes"
+import { StatusCodes } from "../utils/constant"
 
-export const createOrganiserController = async (req: Request, res: Response) => {
-    try {
-        const connection = await pool.connect()
-        const create = await connection
-            .request()
-            .input("organiser_uuid", mssql.UniqueIdentifier, req.body.organiser_uuid)
-            .input("parent_uuid", mssql.UniqueIdentifier, req.body.parent_uuid)
-            .input("organiser_name", mssql.VarChar, req.body.organiser_name)
-            .input("stu_fire_id", mssql.VarChar, req.body.stu_fire_id)
-            .query(`
+export const createOrganiserController = async (
+	req: Request,
+	res: Response
+) => {
+	try {
+		const connection = await pool.connect()
+		const create = await connection
+			.request()
+			.input(
+				"organiser_uuid",
+				mssql.UniqueIdentifier,
+				req.body.organiser_uuid
+			)
+			.input("parent_uuid", mssql.UniqueIdentifier, req.body.parent_uuid)
+			.input("organiser_name", mssql.VarChar, req.body.organiser_name)
+			.input("stu_fire_id", mssql.VarChar, req.body.stu_fire_id).query(`
                 INSERT INTO [sems_demo].[dbo].[tbl_organiser] VALUES (
                     @organiser_uuid,
                     @parent_uuid,
@@ -20,23 +26,30 @@ export const createOrganiserController = async (req: Request, res: Response) => 
                     @stu_fire_id
                 )
             `)
-        res.send(create)
-    } catch (error) {
-        res.status(StatusCodes.BAD_REQUEST).send(error)
-    }
+		res.send(create)
+	} catch (error) {
+		res.status(StatusCodes.BAD_REQUEST).send(error)
+	}
 }
 
-export const updateOrganiserController = async (req: Request, res: Response) => {
-    try {
-        const connection = await pool.connect()
-        const updated = await connection
-            .request()
-            .input("organiser_uuid", mssql.UniqueIdentifier, req.body.organiser_uuid)
-            .input("parent_uuid", mssql.UniqueIdentifier, req.body.parent_uuid)
-            .input("organiser_name", mssql.VarChar, req.body.organiser_name)
-            .input("stu_fire_id", mssql.VarChar, req.body.stu_fire_id)
-            .query(
-                `UPDATE [sems_demo].[dbo].[tbl_organiser] 
+export const updateOrganiserController = async (
+	req: Request,
+	res: Response
+) => {
+	try {
+		const connection = await pool.connect()
+		const updated = await connection
+			.request()
+			.input(
+				"organiser_uuid",
+				mssql.UniqueIdentifier,
+				req.body.organiser_uuid
+			)
+			.input("parent_uuid", mssql.UniqueIdentifier, req.body.parent_uuid)
+			.input("organiser_name", mssql.VarChar, req.body.organiser_name)
+			.input("stu_fire_id", mssql.VarChar, req.body.stu_fire_id)
+			.query(
+				`UPDATE [sems_demo].[dbo].[tbl_organiser] 
 			        SET [parent_uuid] = @parent_uuid, 
                         [organiser_name] = @organiser_name,
                         [stu_fire_id] = @stu_fire_id
@@ -44,27 +57,35 @@ export const updateOrganiserController = async (req: Request, res: Response) => 
 			    
                 SELECT * FROM [sems_demo].[dbo].[tbl_organiser]
 			    WHERE [organiser_uuid] = @organiser_uuid
-			`)
-        return res.send(updated)
-    } catch (error) {
-        res.status(StatusCodes.BAD_REQUEST).send(error)
-    }
+			`
+			)
+		return res.send(updated)
+	} catch (error) {
+		res.status(StatusCodes.BAD_REQUEST).send(error)
+	}
 }
 
-export const deleteOrganiserController = async (req: Request, res: Response) => {
+export const deleteOrganiserController = async (
+	req: Request,
+	res: Response
+) => {
 	try {
 		const connection = await pool.connect()
 		const deleted = await connection
-            .request()
-            .input("organiser_uuid", mssql.UniqueIdentifier, req.body.organiser_uuid)
-            .query(
-                `DELETE [sems_demo].[dbo].[tbl_organiser]
+			.request()
+			.input(
+				"organiser_uuid",
+				mssql.UniqueIdentifier,
+				req.body.organiser_uuid
+			)
+			.query(
+				`DELETE [sems_demo].[dbo].[tbl_organiser]
 			WHERE [organiser_uuid] = @organiser_uuid`
-            )
+			)
 		connection.close()
 		return res.send(deleted)
 	} catch (error) {
-        res.status(StatusCodes.BAD_REQUEST).send(error)
+		res.status(StatusCodes.BAD_REQUEST).send(error)
 	}
 }
 
@@ -72,16 +93,21 @@ export const getOrganisersController = async (req: Request, res: Response) => {
 	try {
 		const connection = await pool.connect()
 		const organisers = await connection.query(
-            `SELECT * FROM [sems_demo].[dbo].[tbl_organiser]`
-        )
+			`SELECT * FROM [sems_demo].[dbo].[tbl_organiser]`
+		)
 		connection.close()
-		return res.status(StatusCodes.OK).json({ records: organisers.recordset })
+		return res
+			.status(StatusCodes.OK)
+			.json({ records: organisers.recordset })
 	} catch (error) {
-        res.status(StatusCodes.BAD_REQUEST).send(error)
+		res.status(StatusCodes.BAD_REQUEST).send(error)
 	}
 }
 
-export const getOrganiserByIDController = async (req: Request, res: Response) => {
+export const getOrganiserByIDController = async (
+	req: Request,
+	res: Response
+) => {
 	try {
 		const connection = await pool.connect()
 		const organiser = await connection
@@ -90,8 +116,8 @@ export const getOrganiserByIDController = async (req: Request, res: Response) =>
 			.query(
 				"SELECT * FROM [sems_demo].[dbo].[tbl_organiser] WHERE organiser_uuid = @organiser_uuid"
 			)
-            connection.close()
-            return res.status(StatusCodes.OK).send(organiser.recordset)
+		connection.close()
+		return res.status(StatusCodes.OK).send(organiser.recordset)
 	} catch (error) {
 		res.status(404).send(error)
 	}
