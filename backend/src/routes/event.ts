aa4fd2ef2991139c7c31b5936150f98e4764992f
wlim0053 <wlim0053@student.monash.edu>
2023-05-11 06:57:18 +0800

    get participants by event

--- backend/src/routes/event.ts
+++ backend/src/routes/event.ts
@@ -5,16 +5,17 @@ import {
 	getEventByIDController,
 	updateOrganiserController,
 	deleteEventController,
+	getEventParticipationController,
 } from "../controllers/eventController"
 
 export const eventRouter = express.Router()
 
-eventRouter
-	.route("/")
-	.post(createEventController)
-	.get(getEventController)
+eventRouter.route("/").post(createEventController).get(getEventController)
 
-    eventRouter
+// * Endpoint to get all the participants for an event
+eventRouter.route("/:id/participants").get(getEventParticipationController)
+
+eventRouter
 	.route("/:id")
 	.put(updateOrganiserController)
 	.get(getEventByIDController)
