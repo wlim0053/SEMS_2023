f33f026d39425665a738bc4df7b231d6469be2cf
Mior Mior Saifullizan <mmio0004@student.monash.edu>
2023-05-11 23:10:03 +0800

    Modified Navbar code to the newest version

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -10,48 +10,66 @@ import {
   DrawerContent,
   DrawerCloseButton,
   useDisclosure,
-  background,
+  VStack,
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuItem,
+  Image,
+  Flex,
+  Spacer,
 } from "@chakra-ui/react";
 import { HamburgerIcon } from "@chakra-ui/icons";
+import { Link, Route, Routes } from "react-router-dom";
 
 const Navbar = () => {
   const { isOpen, onOpen, onClose } = useDisclosure()
   const buttonRef = React.useRef(null)
 
+
   return (
     <>
-      <Box bg="blue.700">
-        {/* <Button ref={buttonRef} onClick={onOpen} variant='ghost' _hover={{}}> */}
-          <HamburgerIcon ref={buttonRef} 
-          onClick={onOpen}
-          boxSize='1.5em'
-          css={{
-            margin: '10px',
-            cursor: 'pointer'
-          }}/> 
-        {/* </Button> */}
-        <Drawer
-          isOpen={isOpen}
-          placement='left'
-          onClose={onClose}
-          finalFocusRef={buttonRef}
-          colorScheme="blackAlpha"
-          >
-          <DrawerOverlay ></DrawerOverlay>
-          <DrawerContent>
-            <DrawerCloseButton/>
-            <DrawerHeader>Menu</DrawerHeader>
-            <DrawerBody>
-              {['Home', 'Organiser List', 'Activity Log'].map((nav) => 
-                <Box>
-                    {nav}
-                </Box>
-              )}
-            </DrawerBody>
-            <DrawerFooter>Hello3</DrawerFooter>
-          </DrawerContent>
-        </Drawer>
-      </Box>
+      {/* Change the gap between hamburger icon and the logo */}
+      <Flex bg="#006DAE" h="60px" alignItems='center' gap='10'>
+        <Box>
+          <HamburgerIcon ref={buttonRef}
+            onClick={onOpen}
+            boxSize='1.5em'
+            color='#FFFFFF'
+            css={{
+              margin: '20px',
+              cursor: 'pointer'
+            }} />
+        </Box>
+        {/* <Spacer/> */}
+        <Image src='/monash_logo.png' height='50px'></Image>
+      </Flex>
+
+
+      {/* </Button> */}
+      <Drawer
+        isOpen={isOpen}
+        placement='left'
+        onClose={onClose}
+        finalFocusRef={buttonRef}
+        colorScheme="blackAlpha"
+      >
+        <DrawerOverlay ></DrawerOverlay>
+        <DrawerContent>
+          <DrawerCloseButton />
+          <DrawerHeader>Menu</DrawerHeader>
+          <DrawerBody>
+            <VStack align="start" spacing={4}>
+              <Link to="/Admin">Home</Link>
+              <Link to="/OrganiserList">Organiser List</Link>
+              <Link to="/ActivityLog">Activity Log</Link>
+              <Link to="/StudentHome">Student Home</Link>
+              <Link to="/EventHome">Event Home</Link>
+            </VStack>
+          </DrawerBody>
+          <DrawerFooter>Hello3</DrawerFooter>
+        </DrawerContent>
+      </Drawer>
     </>
   );
 };
