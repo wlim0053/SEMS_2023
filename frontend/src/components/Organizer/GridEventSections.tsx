3512a782cf29ceda346f585814ddccea40413e8a
Azhan <mame0009@student.monash.edu>
2023-05-23 13:58:07 +0530

    update

--- frontend/src/components/Organizer/GridEventSections.tsx
+++ frontend/src/components/Organizer/GridEventSections.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Box } from '@chakra-ui/react';
 import { Text } from '@chakra-ui/react';
 import { IconButton } from '@chakra-ui/react';
-import { DeleteIcon, RepeatIcon, ViewIcon } from '@chakra-ui/icons';
+import { DeleteIcon, EditIcon, ViewIcon } from '@chakra-ui/icons';
 
 interface EventData {
   id: string;
@@ -18,6 +18,13 @@ interface GridEventSectionsProps {
 }
 
 function GridEventSections({ data }: GridEventSectionsProps) {
+  const handleViewClick = () => {
+    window.location.href = '/EventDetailsDashboard';
+  };
+
+  const handleEditClick = () => {
+    window.location.href = '/CreateEventForm';
+  };
   return (
     <>
       {data.map((event, index) => (
@@ -48,8 +55,8 @@ function GridEventSections({ data }: GridEventSectionsProps) {
             </Text>
           </Box>
           <Box w="100%" h="71" bg={index % 2 === 0 ? '#FFFFFF' : '#EDEEEE'} display="flex" justifyContent={['center', 'space-between']} alignItems={'center'} pl={'45px'} pr={'45px'}>
-            <IconButton colorScheme="blue" aria-label="View Event" icon={<ViewIcon />} size={'sm'} />
-            <IconButton colorScheme="blue" aria-label="Reorganise Event" icon={<RepeatIcon />} size={'sm'} />
+            <IconButton colorScheme="blue" aria-label="View Event" icon={<ViewIcon />} size={'sm'} onClick={handleViewClick}/>
+            <IconButton colorScheme="blue" aria-label="Edit Event" icon={<EditIcon />} size={'sm'} onClick={handleEditClick}/>
             <IconButton colorScheme="blue" aria-label="Delete Event" icon={<DeleteIcon />} size={'sm'} />
           </Box>
         </React.Fragment>
