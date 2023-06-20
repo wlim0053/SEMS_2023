d10436b294150a0f50441eb04d97759941fed43a
wlim0053 <wlim0053@student.monash.edu>
2023-06-21 06:36:00 +0800

    completed certificate generation

--- backend/src/controllers/downloadController.ts
+++ backend/src/controllers/downloadController.ts
@@ -9,17 +9,6 @@ export const downloadCertificateController = async (
 	next: NextFunction
 ) => {
 	try {
-		const titleFont = path.resolve(
-			"static",
-			"fonts",
-			"EdwardianScriptITC.ttf"
-		)
-		const signatureFont = path.resolve(
-			"static",
-			"fonts",
-			"LucidaHandwritingItalic.ttf"
-		)
-
 		// setting document metadata
 		const doc = new PDFDocument({
 			size: "A4",
@@ -30,63 +19,95 @@ export const downloadCertificateController = async (
 			},
 		})
 
-		// register font
-		doc.registerFont("title", titleFont)
-		doc.registerFont("signature", signatureFont)
 		doc.pipe(res) // HTTP response
 		// add stuff to PDF here
-		doc.image(
-			path.resolve("static", "images", "monash_malaysia.png"),
-			35,
-			doc.y,
-			{
-				scale: 0.75,
-			}
-		).moveDown(2)
+		doc.registerFont("Arial", path.resolve("static", "fonts", "arial.TTF"))
+		doc.registerFont(
+			"Arial Bold",
+			path.resolve("static", "fonts", "arial-bold.TTF")
+		)
+		doc.registerFont(
+			"Arial Italic",
+			path.resolve("static", "fonts", "arial-italic.TTF")
+		)
 
-		doc.fontSize(50)
-			.font("title")
-			.text("Certificate of Participation", {
-				align: "center",
-			})
+		doc.image(path.resolve("static", "images", "cert_header.jpg"), 0, 0, {
+			scale: 0.24,
+		}).moveDown(14)
+
+		doc.fontSize(24)
+			.font("Arial")
+			.text("Certificate of Participation", { align: "center" })
 			.moveDown(1)
 
-		doc.font("Times-Roman")
-			.fontSize(20)
+		doc.fontSize(16)
 			.text("This certifies that", { align: "center" })
 			.moveDown(1)
 
-		doc.fontSize(25)
-			.text("Tunku Abdul Rahman ", {
+		doc.font("Arial Bold")
+			.fontSize(20)
+			.text("Tunku Abdul Rahman", {
 				align: "center",
-				underline: false,
+				// underline: true,
 			})
+			.moveDown(0.5)
+
+		doc.font("Arial")
+			.fontSize(16)
+			.text("has attended the", { align: "center" })
 			.moveDown(1)
 
-		doc.fontSize(20)
-			.text("has attended the", { align: "center" })
-			.moveDown(0.5)
-		doc.text("Merdeka Parade", { align: "center" }).moveDown(0.5)
-		doc.text("held on", { align: "center" }).moveDown(0.5)
-		doc.text("31 August 1957", { align: "center" }).moveDown(5)
-
-		let y = doc.y
-
-		// left signature
-		doc.font("signature")
-			.fontSize(20)
-			.text("Safeiya Musafa Hussein", { columns: 2, align: "center" })
-		doc.font("Times-Roman")
-			.fontSize(11)
+		doc.font("Arial Bold")
+			.fontSize(25)
 			.text(
-				"Safeiya Musafa Hussein\n President\n Monash University Malaysia Engineering Clubs (MUMEC)\n\n\n\n",
+				`Career Talk By Vitrox "Resume VS Attitude" Sustainability, green walk + star walk + penang bridge 42km full marathan run`,
 				{
-					columns: 2,
 					align: "center",
 				}
 			)
+			.moveDown(0.5)
 
-		doc.text("Yeap Lai Hoon", { columns: 2, align: "center" })
+		doc.font("Arial")
+			.fontSize(16)
+			.text("held on 31 August 1957", { align: "center" })
+			.moveDown(5)
+
+		const signatureY = 614
+
+		const width = (doc.page.width - 2 * doc.x) / 2 // (width - l & r margins) / 2
+		doc.rect(doc.x, signatureY, width, 100).stroke()
+		doc.rect(doc.page.width / 2, signatureY, width, 100).stroke()
+
+		// left signature
+		doc.font("Arial Bold")
+			.fontSize(12)
+			.text("Safeiya Mustafa Hussein", doc.x, signatureY + 100, {
+				width,
+				align: "center",
+			})
+		doc.font("Arial Italic")
+			.fontSize(12)
+			.text(
+				"President\n Monash University Malaysia Engineering Clubs (MUMEC)",
+				{ width, align: "center" }
+			)
+
+		// right signature
+		doc.font("Arial Bold")
+			.fontSize(12)
+			.text("Yeap Lai Hoon", doc.page.width / 2, signatureY + 100, {
+				width,
+				align: "center",
+			})
+		doc.font("Arial Italic")
+			.fontSize(12)
+			.text(
+				"Manager\n Student Experience and Engagement Monash University Malaysia",
+				{
+					width,
+					align: "center",
+				}
+			)
 
 		// finalise the PDF and end the stream
 		doc.end()
