6367333b406e89d27a802c3395642dd4465fc8e2
wlim0053 <wlim0053@student.monash.edu>
2023-06-22 08:05:56 +0800

    update participation endpoint based on changes in schema and requirements

--- backend/src/interfaces/participation.ts
+++ backend/src/interfaces/participation.ts
@@ -1,5 +1,7 @@
 import zod from "zod"
+import { Event } from "./event"
 
+// * Response body
 export const Participation = zod.object({
 	event_uuid: zod.string().uuid(),
 	user_fire_id: zod.string(),
@@ -8,12 +10,31 @@ export const Participation = zod.object({
 		.refine((value) => value === 0 || value === 1, {
 			message: "Invalid number. Expected 0 or 1.",
 		}),
+	participation_year: zod.number(),
+	participation_semester: zod
+		.number()
+		.refine((value) => value === 1 || value === 2, {
+			message: "Invalid number. Expected 1 or 2.",
+		}),
 })
 
 export const ParticipationWithUUID = Participation.extend({
 	participation_uuid: zod.string().uuid(),
 })
 
+export const ParticipationWithEvent = ParticipationWithUUID.merge(Event)
+
 export type Participation = zod.infer<typeof Participation>
 
 export type ParticipationWithUUID = zod.infer<typeof ParticipationWithUUID>
+
+export type ParticipationWithEvent = zod.infer<typeof ParticipationWithEvent>
+
+// * Query params
+export const ParticipationQueryParams = zod.object({
+	event_status: zod.enum(["D", "P", "A", "R", "C"]).optional(),
+})
+
+export type ParticipationQueryParams = zod.infer<
+	typeof ParticipationQueryParams
+>
--- backend/src/routes/participation.ts
+++ backend/src/routes/participation.ts
@@ -1,12 +1,15 @@
 import express from "express"
 import {
 	createParticipationController,
-	updateParticipationController,
+	markParticipationAttendanceController,
 	getParticipationController,
-	getParticipationByIdController,
-	deleteParticipationController,
+	// getParticipationByIdController,
+	// deleteParticipationController,
 } from "../controllers/participationController"
-import { Participation } from "../interfaces/participation"
+import {
+	Participation,
+	ParticipationQueryParams,
+} from "../interfaces/participation"
 import { requestValidators } from "../middlewares/requestValidator"
 
 export const participationRouter = express.Router()
@@ -17,13 +20,16 @@ participationRouter
 		requestValidators({ body: Participation }),
 		createParticipationController
 	)
-	.get(getParticipationController)
-
-participationRouter
-	.route("/:id")
-	.put(
-		requestValidators({ body: Participation }),
-		updateParticipationController
+	.get(
+		requestValidators({ query: ParticipationQueryParams }),
+		getParticipationController
 	)
-	.get(getParticipationByIdController)
-	.delete(deleteParticipationController)
+
+participationRouter.route("/:id/attendance").patch(
+	requestValidators({
+		body: Participation.pick({ participation_attendance: true }),
+	}),
+	markParticipationAttendanceController
+)
+// .get(getParticipationByIdController)
+// .delete(deleteParticipationController)
