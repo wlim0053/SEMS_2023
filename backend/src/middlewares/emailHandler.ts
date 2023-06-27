c9b9e28570353ae5e7d0176ed1745b9bcc4a9c75
Xue Qi Lee <xueqi0731@gmail.com>
2023-06-27 17:43:58 +0800

    automate sends registration email after participation is created

--- backend/src/middlewares/emailHandler.ts
+++ backend/src/middlewares/emailHandler.ts
@@ -2,7 +2,16 @@ import nodemailer from "nodemailer"
 import { NextFunction, Request, Response } from "express"
 import { StatusCodes } from "../utils/constant"
 import { readFile } from "fs"
-import handlebars from 'handlebars';
+import handlebars from 'handlebars'
+import {
+	EventWithUUID
+} from "../interfaces/event"
+import {
+	UserWithFireId
+} from "../interfaces/user"
+import mssql from "mssql"
+import { pool } from "../utils/dbConfig"
+import { DbTables } from "../utils/constant"
 
 const user = process.env.email_user;
 const pass = process.env.email_password;
@@ -18,6 +27,26 @@ export const registrationEmail = async (req: Request, res: Response, next: NextF
 
     let transporter = nodemailer.createTransport(trans_obj);
 
+    const connection = await pool.connect()
+    const event: mssql.IResult<EventWithUUID> = await connection
+			.request()
+			.input("event_uuid", mssql.UniqueIdentifier, req.body.event_uuid)
+			.query(`
+                SELECT * FROM ${DbTables.EVENT}
+                WHERE event_uuid=@event_uuid
+            `)
+    const eventData = event.recordset[0]; // only single value
+
+    const user_example: mssql.IResult<UserWithFireId> = await connection
+			.request()
+			.input("user_fire_id", mssql.VarChar, req.body.user.user_fire_id)
+			.query(`
+                SELECT * FROM ${DbTables.USER} 
+                WHERE user_fire_id=@user_fire_id
+            `)
+    const userData = user_example.recordset[0]; // only single value
+	connection.close()
+
     readFile('./src/utils/template/registration.html', 'utf8', (err, htmlTemplate) => {
         if (err) {
             console.error('Error reading HTML file:', err);
@@ -25,24 +54,19 @@ export const registrationEmail = async (req: Request, res: Response, next: NextF
         }
       
         const template = handlebars.compile(htmlTemplate);
-      
+        
         const data = {
-            name: 'John Doe',
-            eventName: 'Tech Talk: Exploring the Future of Artificial Intelligence',
+            name: userData.user_fname.toUpperCase() + " " + userData.user_lname.toUpperCase(),
+            eventName: eventData.event_title.toUpperCase(),
         };
       
         const mail = template(data);
       
         let message = {
             from: user,
-            to: ["xlee0024@student.monash.edu"],
-            subject: "Thank you for your registration!",
+            to: [userData.user_email],
+            subject: "Thank you for your registration!!",
             html: mail
-            // ,
-            // attachments: [{
-            //     filename: "2023-Engaged_and_Integrated_Student_Experience_Platform.pdf",
-            //     path: './src/2023-Engaged_and_Integrated_Student_Experience_Platform.pdf'
-            // }]
         }
       
         transporter.sendMail(message).then(() => {
