f89d67e62a83741c19e2778ed2a38217d45c2880
wlim0053 <91792055+wlim0053@users.noreply.github.com>
2023-07-03 21:39:11 +0800

    Anson w landingpage student (#20)
    * Removed old history page
    
    * Set up functions to sort and search
    
    * Added frontend table for history page
    
    * Added toast for feedback button
    
    * Added a few documentation
    
    * Revamped landing page
    
    * Testing webpage
    
    * Changed colour
    
    * Trying other designs
    
    * Testing design
    
    * Finished with new design
    
    * Test localhost
    
    * Changed button position
    
    * Removed last page
    
    * Changed to correct area
    
    * Removed redundant codes
    
    * Added and adjusted new group picture
    
    * corrected alignment
    
    * merging branch
    
    * Testing codes
    
    * Adjusted alignment
    
    * Removed testing
    
    * Removed old history page
    
    ---------
    
    Co-authored-by: Anson Wong Sie Yuan <awon0048@student.monash.edu>

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -13,6 +13,12 @@ import NewHistoryPage from "./components/NewHistoryPage";
 // import OrganiserList from "./pages/admin/OrganiserList";
 // import ActivityLog from "./pages/admin/ActivityLog";
 
+// testing
+import Testlandingpage from "./components/Testlandingpage"
+import StudentEvents from "./components/StudentEvents"
+import NewLandingPage from "./components/NewLandingPage"
+import Testing from "./components/Testing"
+
 function App() {
   return (
     <div className="App">
