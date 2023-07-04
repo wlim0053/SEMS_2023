e4af6bf529e401182c27d5a65984a625af02492d
wlim0053 <91792055+wlim0053@users.noreply.github.com>
2023-07-04 15:07:57 +0800

    Revert "Demotrial (#27)"
    This reverts commit a77c7bbd43923d46d27f9ae2f0c7bf9dd0df4092.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,17 +7,13 @@ import { ChakraProvider } from "@chakra-ui/react";
 // components
 // css
 import "./App.css";
-import { BrowserRouter as Router, Route, Routes, Outlet } from "react-router-dom";
-import NewHistoryPage from "./components/NewHistoryPage";
+import { BrowserRouter as Router, Route, Outlet } from "react-router-dom";
 // import OrganiserList from "./pages/admin/OrganiserList";
 // import ActivityLog from "./pages/admin/ActivityLog";
 
 // testing
-import OrganiserMainPage from "./pages/Organiser/OrganiserMainPage"
-
-import StudentEvents from "./components/StudentEvents"
-import NewLandingPage from "./components/NewLandingPage"
-
+import OrganiserMainPage from "./pages/Organiser/OrganiserMainPage";
+import StudentEvents from "./components/StudentEvents";
 
 function App() {
   return (
