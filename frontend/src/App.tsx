e43090dbce669200e7ebe0e6a9808311b0c1dbd0
KHOO KAI NENG <kkho0020@student.monash.edu>
2023-07-04 11:24:36 +0800

    organised pages and components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,23 +2,18 @@
 // components
 import Navbar from "./components/shared/Navbar";
 import Admin from "./pages/Admin/AdminDashboard";
-import RegisterPage from "./components/RegisterPage";
 
 import { ChakraProvider } from "@chakra-ui/react";
 // components
 // css
 import "./App.css";
 import { BrowserRouter as Router, Route, Outlet } from "react-router-dom";
-import NewHistoryPage from "./components/NewHistoryPage";
 // import OrganiserList from "./pages/admin/OrganiserList";
 // import ActivityLog from "./pages/admin/ActivityLog";
 
 // testing
-import OrganiserMainPage from "./pages/Organiser/OrganiserMainPage"
-import Testlandingpage from "./components/Testlandingpage"
-import StudentEvents from "./components/StudentEvents"
-import NewLandingPage from "./components/NewLandingPage"
-import Testing from "./components/Testing"
+import OrganiserMainPage from "./pages/Organiser/OrganiserMainPage";
+import StudentEvents from "./components/StudentEvents";
 
 function App() {
   return (
@@ -26,7 +21,6 @@ function App() {
       <Router>
         <Navbar />
         <Outlet /> {/* Render nested routes */}
-        {/* <NewHistoryPage /> */}
       </Router>
     </div>
   );
