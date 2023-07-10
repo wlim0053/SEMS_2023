a1355f2552ff7dd2c9ab892d0f9a2d730dbce2b4
Hoh Zheng Jie <zhoh0001@student.monash.edu>
2023-07-10 23:31:39 +0800

    Reminder Email and Certificate Sender Functionality Added

--- frontend/src/components/Organizer/GridEventSections.tsx
+++ frontend/src/components/Organizer/GridEventSections.tsx
@@ -10,8 +10,17 @@ import {
   AlertDialogOverlay,
   Box,
   IconButton,
+  Grid,
+  GridItem,
 } from "@chakra-ui/react";
-import { DeleteIcon, EditIcon, ViewIcon, CheckIcon } from "@chakra-ui/icons";
+import {
+  DeleteIcon,
+  EditIcon,
+  ViewIcon,
+  CheckIcon,
+  StarIcon,
+  EmailIcon,
+} from "@chakra-ui/icons";
 import { useNavigate } from "react-router-dom";
 import api from "../../utils/api";
 import React from "react";
@@ -47,11 +56,30 @@ function GridEventSections({ data, setRefreshGrid }: GridEventSectionsProps) {
   const navigate = useNavigate();
   const [selectedEventUUID, setSelectedEventUUID] = useState("");
 
-  const [isDeleteConfirmationOpen, setIsDeleteConfirmationOpen] = useState(false);
+  const [isDeleteConfirmationOpen, setIsDeleteConfirmationOpen] =
+    useState(false);
   const onDeleteConfirmationClose = () => setIsDeleteConfirmationOpen(false);
 
-  const [isMarkAsCompletedConfirmationOpen, setIsMarkAsCompletedConfirmationOpen] = useState(false);
-  const onMarkAsCompletedConfirmationClose = () => setIsMarkAsCompletedConfirmationOpen(false);
+  const [
+    isMarkAsCompletedConfirmationOpen,
+    setIsMarkAsCompletedConfirmationOpen,
+  ] = useState(false);
+  const onMarkAsCompletedConfirmationClose = () =>
+    setIsMarkAsCompletedConfirmationOpen(false);
+
+  const [
+    isSendCertificateConfirmationOpen,
+    setIsSendCertificateConfirmationOpen,
+  ] = useState(false);
+  const onSendCertificateConfirmationClose = () =>
+    setIsSendCertificateConfirmationOpen(false);
+
+  const [
+    isSendEmailConfirmationOpen,
+    setIsSendEmailConfirmationOpen,
+  ] = useState(false);
+  const onSendEmailConfirmationClose = () =>
+    setIsSendEmailConfirmationOpen(false);
 
   const cancelRef = useRef<HTMLButtonElement>(null);
 
@@ -69,8 +97,6 @@ function GridEventSections({ data, setRefreshGrid }: GridEventSectionsProps) {
     console.log("Event UUID: " + eventDataUUID);
     navigate("/EditEventPage", { state: { eventDataUUID } });
   };
-
-
 
   const deleteEventFromDatabase = async () => {
     try {
@@ -99,7 +125,9 @@ function GridEventSections({ data, setRefreshGrid }: GridEventSectionsProps) {
 
   const markEventAsCompletedInDatabase = async () => {
     try {
-      let response = await api.patch(`/event/for-organiser/${selectedEventUUID}/complete`);
+      let response = await api.patch(
+        `/event/for-organiser/${selectedEventUUID}/complete`
+      );
       console.log(response.data); // Response data from the server
       // Handle the response or perform any necessary actions
       setRefreshGrid(true); // Trigger refresh of events data of grid
@@ -123,6 +151,62 @@ function GridEventSections({ data, setRefreshGrid }: GridEventSectionsProps) {
     markEventAsCompletedInDatabase();
     setRefreshGrid(true); // Trigger refresh of events data of grid
     onDeleteConfirmationClose(); // Close the alert dialog after form deletion
+  };
+
+  const sendCertificate = async () => {
+    try {
+      let response = await api.patch(
+        `/event/for-organiser/${selectedEventUUID}/cert`
+      );
+      console.log(response.data); // Response data from the server
+      // Handle the response or perform any necessary actions
+    } catch (error) {
+      console.log(`Error: ${error}`);
+      // Handle the error appropriately
+    }
+  };
+
+  const handleSendCertificateClick = async (
+    event: React.MouseEvent<HTMLButtonElement>
+  ) => {
+    const index = Number(event.currentTarget.getAttribute("data-index"));
+    const certificatedEventUUID = data[index].event_uuid;
+    setSelectedEventUUID(certificatedEventUUID);
+    console.log("Selected Event UUID: ", data[index]);
+    setIsSendCertificateConfirmationOpen(true);
+  };
+
+  const handleConfirmSendCertificateClick = () => {
+    sendCertificate();
+    onSendCertificateConfirmationClose(); // Close the alert dialog after form deletion
+  };
+
+  const sendReminderEmail = async () => {
+    try {
+      let response = await api.patch(
+        `/event/for-organiser/${selectedEventUUID}/reminder`
+      );
+      console.log(response.data); // Response data from the server
+      // Handle the response or perform any necessary actions
+    } catch (error) {
+      console.log(`Error: ${error}`);
+      // Handle the error appropriately
+    }
+  };
+
+  const handleReminderEmailClick = async (
+    event: React.MouseEvent<HTMLButtonElement>
+  ) => {
+    const index = Number(event.currentTarget.getAttribute("data-index"));
+    const emailedEventUUID = data[index].event_uuid;
+    setSelectedEventUUID(emailedEventUUID);
+    console.log("Selected Event UUID: ", data[index]);
+    setIsSendEmailConfirmationOpen(true);
+  };
+
+  const handleConfirmReminderEmailClick = () => {
+    sendReminderEmail();
+    onSendEmailConfirmationClose(); // Close the alert dialog after form deletion
   };
 
   const dateFormatter = (event_start_date: string): string => {
@@ -172,7 +256,7 @@ function GridEventSections({ data, setRefreshGrid }: GridEventSectionsProps) {
         <React.Fragment key={index}>
           <Box
             w="100%"
-            h="71"
+            h="76"
             bg={index % 2 === 0 ? "#FFFFFF" : "#EDEEEE"}
             display="flex"
             justifyContent={"center"}
@@ -196,7 +280,7 @@ function GridEventSections({ data, setRefreshGrid }: GridEventSectionsProps) {
           </Box>
           <Box
             w="100%"
-            h="71"
+            h="76"
             bg={index % 2 === 0 ? "#FFFFFF" : "#EDEEEE"}
             display="flex"
             justifyContent={"center"}
@@ -208,7 +292,7 @@ function GridEventSections({ data, setRefreshGrid }: GridEventSectionsProps) {
           </Box>
           <Box
             w="100%"
-            h="71"
+            h="76"
             bg={index % 2 === 0 ? "#FFFFFF" : "#EDEEEE"}
             display="flex"
             justifyContent={"center"}
@@ -220,7 +304,7 @@ function GridEventSections({ data, setRefreshGrid }: GridEventSectionsProps) {
           </Box>
           <Box
             w="100%"
-            h="71"
+            h="76"
             bg={index % 2 === 0 ? "#FFFFFF" : "#EDEEEE"}
             display="flex"
             justifyContent={"center"}
@@ -232,7 +316,7 @@ function GridEventSections({ data, setRefreshGrid }: GridEventSectionsProps) {
           </Box>
           <Box
             w="100%"
-            h="71"
+            h="76"
             bg={index % 2 === 0 ? "#FFFFFF" : "#EDEEEE"}
             display="flex"
             justifyContent={"center"}
@@ -244,55 +328,95 @@ function GridEventSections({ data, setRefreshGrid }: GridEventSectionsProps) {
           </Box>
           <Box
             w="100%"
-            h="71"
+            h="76"
             bg={index % 2 === 0 ? "#FFFFFF" : "#EDEEEE"}
             display="flex"
             justifyContent={["center", "space-between"]}
             alignItems={"center"}
             pl={"45px"}
             pr={"45px"}
+            pt={"15px"}
+            pb={"15px"}
           >
-            <IconButton
-              colorScheme="blue"
-              aria-label="View Event"
-              icon={<ViewIcon />}
-              size={"sm"}
-              onClick={handleViewClick}
-              data-index={index}
-            />
+            <Grid
+              templateColumns={["repeat(1, 1fr)", "repeat(3, 1fr)"]} // Adjust the number of columns as per your requirement
+              gap={2} // Adjust the gap between buttons if needed
+            >
+              <GridItem colSpan={[1, 1]}>
+                <IconButton
+                  colorScheme="blue"
+                  aria-label="View Event"
+                  icon={<ViewIcon />}
+                  size={"sm"}
+                  onClick={handleViewClick}
+                  data-index={index}
+                />
+              </GridItem>
 
-            {shouldShowEditButton(event.event_status) ? (
-              <IconButton
-                colorScheme="blue"
-                aria-label="Edit Event"
-                icon={<EditIcon />}
-                size={"sm"}
-                onClick={handleEditClick}
-                data-index={index}
-              />
-            ) : null}
+              {shouldShowEditButton(event.event_status) ? (
+                <GridItem colSpan={[1, 1]}>
+                  <IconButton
+                    colorScheme="blue"
+                    aria-label="Edit Event"
+                    icon={<EditIcon />}
+                    size={"sm"}
+                    onClick={handleEditClick}
+                    data-index={index}
+                  />
+                </GridItem>
+              ) : null}
 
-            {shouldShowDeleteButton(event.event_status) ? (
-              <IconButton
-                colorScheme="blue"
-                aria-label="Delete Event"
-                icon={<DeleteIcon />}
-                size={"sm"}
-                onClick={handleDeleteClick}
-                data-index={index}
-              />
-            ) : null}
+              {shouldShowDeleteButton(event.event_status) ? (
+                <GridItem colSpan={[1, 1]}>
+                  <IconButton
+                    colorScheme="blue"
+                    aria-label="Delete Event"
+                    icon={<DeleteIcon />}
+                    size={"sm"}
+                    onClick={handleDeleteClick}
+                    data-index={index}
+                  />
+                </GridItem>
+              ) : null}
 
-            {shouldShowMarkAsCompletedButton(event.event_status, event.event_start_date) ? (
-              <IconButton
-                colorScheme="blue"
-                aria-label="Mark as Completed"
-                icon={<CheckIcon />}
-                size={"sm"}
-                onClick={handleMarkAsCompletedClick}
-                data-index={index}
-              />
-            ) : null}
+              {shouldShowMarkAsCompletedButton(
+                event.event_status,
+                event.event_start_date
+              ) ? (
+                <GridItem colSpan={[1, 1]}>
+                  <IconButton
+                    colorScheme="blue"
+                    aria-label="Mark as Completed"
+                    icon={<CheckIcon />}
+                    size={"sm"}
+                    onClick={handleMarkAsCompletedClick}
+                    data-index={index}
+                  />
+                </GridItem>
+              ) : null}
+
+              <GridItem colSpan={[1, 1]}>
+                <IconButton
+                  colorScheme="blue"
+                  aria-label="Send certificate"
+                  icon={<StarIcon />}
+                  size={"sm"}
+                  onClick={handleSendCertificateClick}
+                  data-index={index}
+                />
+              </GridItem>
+
+              <GridItem colSpan={[1, 1]}>
+                <IconButton
+                  colorScheme="blue"
+                  aria-label="Send certificate"
+                  icon={<EmailIcon />}
+                  size={"sm"}
+                  onClick={handleReminderEmailClick}
+                  data-index={index}
+                />
+              </GridItem>
+            </Grid>
           </Box>
         </React.Fragment>
       ))}
@@ -336,7 +460,10 @@ function GridEventSections({ data, setRefreshGrid }: GridEventSectionsProps) {
               Are you sure you want to mark this event as completed?
             </AlertDialogBody>
             <AlertDialogFooter>
-              <Button ref={cancelRef} onClick={onMarkAsCompletedConfirmationClose}>
+              <Button
+                ref={cancelRef}
+                onClick={onMarkAsCompletedConfirmationClose}
+              >
                 Cancel
               </Button>
               <Button
@@ -345,6 +472,66 @@ function GridEventSections({ data, setRefreshGrid }: GridEventSectionsProps) {
                 onClick={handleConfirmMarkAsCompletedClick}
               >
                 Completed
+              </Button>
+            </AlertDialogFooter>
+          </AlertDialogContent>
+        </AlertDialogOverlay>
+      </AlertDialog>
+
+      <AlertDialog
+        isOpen={isSendEmailConfirmationOpen}
+        leastDestructiveRef={cancelRef}
+        onClose={onSendEmailConfirmationClose}
+      >
+        <AlertDialogOverlay>
+          <AlertDialogContent>
+            <AlertDialogHeader>Send out Reminder Email</AlertDialogHeader>
+            <AlertDialogBody>
+              Are you sure you want to send reminder emails for this event?
+            </AlertDialogBody>
+            <AlertDialogFooter>
+              <Button
+                ref={cancelRef}
+                onClick={onSendEmailConfirmationClose}
+              >
+                Cancel
+              </Button>
+              <Button
+                colorScheme="red"
+                ml={3}
+                onClick={handleConfirmReminderEmailClick}
+              >
+                Send
+              </Button>
+            </AlertDialogFooter>
+          </AlertDialogContent>
+        </AlertDialogOverlay>
+      </AlertDialog>
+
+      <AlertDialog
+        isOpen={isSendCertificateConfirmationOpen}
+        leastDestructiveRef={cancelRef}
+        onClose={onSendCertificateConfirmationClose}
+      >
+        <AlertDialogOverlay>
+          <AlertDialogContent>
+            <AlertDialogHeader>Send out Certificate</AlertDialogHeader>
+            <AlertDialogBody>
+              Are you sure you want to send out the certificates for this event?
+            </AlertDialogBody>
+            <AlertDialogFooter>
+              <Button
+                ref={cancelRef}
+                onClick={onSendCertificateConfirmationClose}
+              >
+                Cancel
+              </Button>
+              <Button
+                colorScheme="red"
+                ml={3}
+                onClick={handleConfirmSendCertificateClick}
+              >
+                Send
               </Button>
             </AlertDialogFooter>
           </AlertDialogContent>
